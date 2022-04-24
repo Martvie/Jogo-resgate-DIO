2c0ed28b01ad4242932fc96edc1bdcb10ab91217
Martvie <marviedias@gmail.com>
2022-04-24 20:46:26 -0300

    animations

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,12 @@
+
+
 function start() {
 
 	$("#inicio").hide();
 	
-	$("#fundoGame").append("<div id='jogador'></div>");
-	$("#fundoGame").append("<div id='inimigo1'></div>");
+	$("#fundoGame").append("<div id='jogador' class='anima1'></div>");
+	$("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
 	$("#fundoGame").append("<div id='inimigo2'></div>");
-	$("#fundoGame").append("<div id='amigo'></div>");
+	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
 } 
