3534e2f51240317029343cda31cf83e3fa151db0
Martvie <marviedias@gmail.com>
2022-04-24 20:34:50 -0300

    js init

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+function start() {
+
+	$("#inicio").hide();
+	
+	$("#fundoGame").append("<div id='jogador'></div>");
+	$("#fundoGame").append("<div id='inimigo1'></div>");
+	$("#fundoGame").append("<div id='inimigo2'></div>");
+	$("#fundoGame").append("<div id='amigo'></div>");
+
+} 
