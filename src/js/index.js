cd305525359505d4f01e87a5934a2386cba20d06
Martvie <marviedias@gmail.com>
2022-04-25 23:48:45 -0300

    helicopter drive

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,34 @@
 var jogo = {}
-	
-jogo.timer = setInterval(loop,30);
+var TECLA = {
+	W: 87, S: 83, D: 68
+}
+
+jogo.pressionou = [];
+
+$(document).keydown(function (e) {
+	jogo.pressionou[e.which] = true;
+});
+
+
+$(document).keyup(function (e) {
+	jogo.pressionou[e.which] = false;
+});
+
+
+jogo.timer = setInterval(loop, 30);
 
 function loop() {
 
-movefundo();
+	movefundo()
+	movejogador()
 
 }
 
 
 function movefundo() {
 
-esquerda = parseInt($("#fundoGame").css("background-position"));
-$("#fundoGame").css("background-position",esquerda-1);
+	esquerda = parseInt($("#fundoGame").css("background-position"));
+	$("#fundoGame").css("background-position", esquerda - 1);
 
 }
 
@@ -20,10 +36,42 @@ $("#fundoGame").css("background-position",esquerda-1);
 function start() {
 
 	$("#inicio").hide();
-	
+
 	$("#fundoGame").append("<div id='jogador' class='anima1'></div>");
 	$("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
 	$("#fundoGame").append("<div id='inimigo2'></div>");
 	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
-} 
+}
+
+function movejogador() {
+
+	if (jogo.pressionou[TECLA.W]) {
+		var topo = parseInt($("#jogador").css("top"));
+		$("#jogador").css("top", topo - 10);
+
+
+		if (topo <= 0) {
+
+			$("#jogador").css("top", topo + 10);
+		}
+
+	}
+
+	if (jogo.pressionou[TECLA.S]) {
+
+		var topo = parseInt($("#jogador").css("top"));
+		$("#jogador").css("top", topo + 10);
+		if (topo >= 434) {
+			$("#jogador").css("top", topo - 10);
+
+		}
+
+	}
+
+	if (jogo.pressionou[TECLA.D]) {
+
+		//Chama função Disparo	
+	}
+
+} // fim da função movejogador()
